55bf04ca7457659773a68628161bd9052e7e508a
mpepe2710333 <mpepe2710@icloud.com>
2026-09-24 19:25:28 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+export const metadata = { title: "Allenamenti Tracker" };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="it">
+      <body>{children}</body>
+    </html>
+  );
+}
